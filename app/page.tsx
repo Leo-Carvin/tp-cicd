a9fac28a524a1096c4c534843e0c4c364c1891f2
LeoCarvin <leocarvin13@gmail.com>
2026-06-10 11:21:16 +0200

    feat: mise à jour du titre

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
  return (
  <main className="flex min-h-screen flex-col items-center justify-center gap-4">
  <h1 className="text-4xl font-bold">Mon premier déploiement CI/CD </h1>
- <p className="text-lg text-gray-500">Déployé automatiquement via GitHub Actions & Vercel</p>
+ <p className="text-lg text-gray-500">Déployé automatiquement via GitHub Actions & Vercel modif test</p>
  </main>
  );
 }
