7b16317586c46bd7272592fe407798f7fb28c395
LeoCarvin <leocarvin13@gmail.com>
2026-06-10 11:28:47 +0200

    Test Echec

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
  <main className="flex min-h-screen flex-col items-center justify-center gap-4">
  <h1 className="text-4xl font-bold">Mon premier déploiement CI/CD </h1>
  <p className="text-lg text-gray-500">Déployé automatiquement via GitHub Actions & Vercel modif test</p>
+ const x: number = "ceci n'est pas un nombre";
  </main>
  );
 }
